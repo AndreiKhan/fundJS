d515f316fd1b9478e03852c7cb4ca0c4e564f805
«Andrei» <Spectacul9r@mail.ru>
2023-07-20 17:16:02 +0500

    les1

--- les1/index.js
+++ les1/index.js
@@ -5,6 +5,8 @@ let fahrenheit = (9/5) * celsius + 32;  // градусы фаренгейт
 
 console.log(celsius + " градусов цельсия = " + fahrenheit + " градусам по фаренгейту");
 
+
+
 //------2 задание------
 
 let name = "Андрей";
