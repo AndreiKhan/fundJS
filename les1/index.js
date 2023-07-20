d065108826b5cb83b7a602ce050ba6cf7710d88c
«Andrei» <Spectacul9r@mail.ru>
2023-07-20 17:15:31 +0500

    les1

--- les1/index.js
+++ les1/index.js
@@ -1,1 +1,13 @@
-console.log("1");
+//------1 задание------
+
+let celsius = 20;   // градусы цельсия
+let fahrenheit = (9/5) * celsius + 32;  // градусы фаренгейт
+
+console.log(celsius + " градусов цельсия = " + fahrenheit + " градусам по фаренгейту");
+
+//------2 задание------
+
+let name = "Андрей";
+let admin = name;
+
+console.log("admin = " + admin);
